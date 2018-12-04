5bc0e533b42f5dc031c14f61936470f60e21e238
Cheng Vue <cvu3@ucsc.edu>
2018-12-04 21:45:23 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,31 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
-  
-    def print_courses
+  def get_page
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  end
+
+  def get_courses
+    get_page.css(".posts")
+  end
+
+  def make_courses
+    get_courses.each do |course|
+      title = course.css("h2").text
+      schedule = course.css(".date").text
+      description = course.css("p").text
+      new_course = Course.new
+      new_course.title = title
+      new_course.schedule = schedule
+      new_course.description = description
+    end
+  end
+
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title
@@ -15,8 +35,4 @@ class Scraper
       end
     end
   end
-  
 end
-
-
-
